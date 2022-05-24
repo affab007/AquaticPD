0f46c8b25fe389c62baa12e815623ece18e8b7ba
affab007 <affab007@gmail.com>
2022-05-24 15:15:28 +0700

    switch pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+@Output() openAquaticFood = new EventEmitter<string>();
+@Output() openDistribution = new EventEmitter<string>();
 
+onChangeAquaticFood(){
+  this.openAquaticFood.emit('Aquatic');
+  // this.openDistribution.emit('off');
+}
+onChangeDistribution(){
+  this.openAquaticFood.emit('Distribution');
+  // this.openDistribution.emit('on');
+}
 }
